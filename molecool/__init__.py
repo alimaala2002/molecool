5617a423d04be9599f8ce7863cc0d687f63c4bd7
alimaala <alimaala@purdue.edu>
2024-09-23 16:03:43 -0300

    Import the Canvas function

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -1,7 +1,7 @@
 """a Python package to visualize molecules given their Cartesian coordinates"""
 
 # Add imports here
-from .functions import *
+from .functions import canvas
 
 
 from ._version import __version__
